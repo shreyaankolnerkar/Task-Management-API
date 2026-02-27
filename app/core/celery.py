e86d423f9a3ec38fb64f069c8fff7e7198877a90
shreyaankolnerkar <shreya.ankolnerkar@gammaedge.io>
2026-02-27 14:26:30 +0530

    Update Celery

--- app/core/celery.py
+++ app/core/celery.py
@@ -2,7 +2,7 @@ from celery import Celery
 import os
 from dotenv import load_dotenv
 load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+REDIS_URL = os.getenv("REDIS_URL")
 
 celery_app = Celery(
     "task_management",
